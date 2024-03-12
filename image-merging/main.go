2cf67a13efa1a643cb6318077df5df35a2b5a37e
sanghvisagar <sanghvi1357@gmail.com>
2024-03-12 09:14:41 +0530

    weighted load balancer

--- image-merging/main.go
+++ image-merging/main.go
@@ -1,28 +1,59 @@
-// node 
-// parent
-// child 
-// value
-
-
-
-
 package main
 
+import "fmt"
 
 type Node struct {
-	Value int
+	Value string
 	//Parent Node
 	Child []Node
 }
 
 func main() {
+	tree1 := Node{
+		Value: "X",
+		Child: []Node{
+			Node{
+				Value: "X",
+				Child: []Node{
+					Node{
+						Value: "1",
+					},
+					Node{
+						Value: "0",
+					},
+					Node{
+						Value: "1",
+					},
+					Node{
+						Value: "1",
+					},},
+			},
+			Node{
+				Value: "1",
+			},
+			Node{
+				Value: "0",
+			},
+			Node{
+				Value: "1",
+			},
+		},
+	}
 
-	tree1 := Node{}
-	tree2 := Node{}
+	// tree2 := Node{
+	// 	Value: 1,
+	// }
 
-	resultTree := Node{}
+	simpleBFS(tree1)
 
-	treeTraversal(tree1, tree2, &resultTree)
+
+
+	// // tree1 := Node{}
+	// // tree2 := Node{}
+
+	// resultTree := Node{}
+
+	// treeTraversal(tree1, tree2, &resultTree)
 
 
 
@@ -30,22 +61,32 @@ func main() {
 
 }
 
-Tree1
-             x
+// Tree1
+//              x
 
-	  x      1    0     1
-     1 0 1 1
-
-
-Tree2 
-              1
+// 	  x      1    0     1
+// 1 0 1 1
 
 
+// Tree2 
+//               1
 
-resultTree
+
+
+// resultTree     
 	
 
+func simpleBFS(root Node) {
+	if root.Child == nil || len(root.Child) == 0 {
+		fmt.Println(root.Value)
+		return
+	}
 
+	fmt.Println(root.Value)
+	for _, child := range root.Child {
+		simpleBFS(child)
+	}
+}
 
               
 
@@ -53,73 +94,73 @@ resultTree
 
 
 
-func treeTraversal(tree1 Node, tree2 Node, resultTree *Node)  {
-	n := len(tree1.Child)
-	m := len(tree2.Child)
+// func treeTraversal(tree1 Node, tree2 Node, resultTree *Node)  {
+// 	n := len(tree1.Child)
+// 	m := len(tree2.Child)
 
-	if n==0 and m==0 {
-		resultTree := Node{
-			Value: operate(tree1.Value, tree2.Value)
-			Child: []Node{}
-		}
-		// resultTree.Child = append(resultTree.Child, newNode)
-		return 
-	}
+// 	if n==0 and m==0 {
+// 		resultTree := Node{
+// 			Value: operate(tree1.Value, tree2.Value)
+// 			Child: []Node{}
+// 		}
+// 		// resultTree.Child = append(resultTree.Child, newNode)
+// 		return 
+// 	}
 
-	for idx, childNode := range max(tree1,tree2) {
+// 	for idx, childNode := range max(tree1,tree2) {
 		
-		if idx >= m {
-			treeTraversal(childNode, Node{Value: tree2.Value}, resultTree.Child[idx])
-		} else {
-			treeTraversal(childNode, tree2.Child[idx],  resultTree.Child[idx])
-		}
-	}
+// 		if idx >= m {
+// 			treeTraversal(childNode, Node{Value: tree2.Value}, resultTree.Child[idx])
+// 		} else {
+// 			treeTraversal(childNode, tree2.Child[idx],  resultTree.Child[idx])
+// 		}
+// 	}
 
-	//if tree1.value == tree2.value {
-	// create a new node - get its value via operate function 
-	//    update the child of existing node 
-	//   update resultTree to new node
-	newNode := Node{
-		Value: operate(tree1.Value, tree2.Value)
-		Child: []Node{}
-	}
-	resultTree.Child = append(resultTree.Child, newNode)
-	// resultTree = newNode
-
-
-	
-
-
-	// if both are nil , just return 
-
-	// if one of tree is nil and other is non nil 
-
-	// if both are non nil
-	// call the operate function
-	// return the value after applying 
-
-	
-
-	// newNode := Node{}
-	// resultTree.value = operate(tree1.val, tree2.val)
-	
-
-
-
-
-
-	// if n>m {
-	
-	// }
-
-
+// 	//if tree1.value == tree2.value {
+// 	// create a new node - get its value via operate function 
+// 	//    update the child of existing node 
+// 	//   update resultTree to new node
+// 	newNode := Node{
+// 		Value: operate(tree1.Value, tree2.Value)
+// 		Child: []Node{}
+// 	}
+// 	resultTree.Child = append(resultTree.Child, newNode)
+// 	// resultTree = newNode
 
 
 	
 
-}
+
+// 	// if both are nil , just return 
+
+// 	// if one of tree is nil and other is non nil 
+
+// 	// if both are non nil
+// 	// call the operate function
+// 	// return the value after applying 
+
+	
+
+// 	// newNode := Node{}
+// 	// resultTree.value = operate(tree1.val, tree2.val)
+	
 
 
-func operate(val1 int, val2 int) {
 
-}
+
+
+// 	// if n>m {
+	
+// 	// }
+
+
+
+
+	
+
+// }
+
+
+// func operate(val1 int, val2 int) {
+
+// }
